481f314c7f6084276b3e43c4cefb1b93d95b71ac
Matthew <110214849+standycore@users.noreply.github.com>
2022-12-11 19:44:49 -0800

    added some libraries

--- main.js
+++ main.js
@@ -1,34 +1,51 @@
 import './style.css';
-import { Application, Assets, Sprite } from 'pixi.js';
 
-// The application will create a renderer using WebGL, if possible,
-// with a fallback to a canvas render. It will also setup the ticker
-// and the root stage PIXI.Container
-const app = new Application();
+import { World } from './lib/world';
 
-// The application will create a canvas element for you that you
-// can then insert into the DOM
-document.body.appendChild(app.view);
+import { Application, Graphics } from 'pixi.js';
 
-// load the texture we need
-const texture = await Assets.load('bunny.png');
+async function main() {
+    // The application will create a renderer using WebGL, if possible,
+    // with a fallback to a canvas render. It will also setup the ticker
+    // and the root stage PIXI.Container
+    const app = new Application({ resizeTo: window });
 
-// This creates a texture from a 'bunny.png' image
-const bunny = new Sprite(texture);
+    // The application will create a canvas element for you that you
+    // can then insert into the DOM
+    document.body.appendChild(app.view);
 
-// Setup the position of the bunny
-bunny.x = app.renderer.width / 2;
-bunny.y = app.renderer.height / 2;
+    const grid = new World();
 
-// Rotate around the center
-bunny.anchor.x = 0.5;
-bunny.anchor.y = 0.5;
+    grid.set(0, 0, 'a');
+    grid.set(1, 0, 'a');
+    grid.set(0, 2, 'a');
+    grid.set(3, 4, 'a');
+    grid.set(2, 6, 'a');
 
-// Add the bunny to the scene we are building
-app.stage.addChild(bunny);
+    const graphics = new Graphics();
 
-// Listen for frame updates
-app.ticker.add(() => {
-    // each frame we spin the bunny around a bit
-    bunny.rotation += 0.01;
-});
+    grid.forEach((value, x, y) => {
+        graphics.drawRect(x * 20, y * 20, 20, 20);
+    });
+
+    grid.cellSize.set(40, 40);
+
+    app.stage.addChild(graphics);
+
+    // let time = 0;
+    // Listen for frame updates
+    app.ticker.add((delta) => {
+        // time += delta;
+
+        graphics.clear();
+
+        graphics.lineStyle(1, 0xffffff);
+
+        // grid.drawGrid(graphics, Math.sin(time * 0.1) * 10, Math.cos(time * 0.1) * 10, app.screen.width, app.screen.height);
+        grid.drawGrid(graphics, 0, 0, app.screen.width, app.screen.height);
+    });
+
+    return 'solemthing';
+}
+
+main();
