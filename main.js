68178d26b392e8804c8ee1ffb808d0e860988d43
Matthew <110214849+standycore@users.noreply.github.com>
2022-12-18 17:01:53 -0800

    implemented somehwat working wave function collapse

--- main.js
+++ main.js
@@ -2,10 +2,13 @@ import './style.css';
 
 import { World } from '$/world';
 
-import { Application, Assets, Container, Graphics, ObservablePoint, Point, RenderTexture, SCALE_MODES, settings, Sprite } from 'pixi.js';
+import { Application, Assets, Container, Graphics, ObservablePoint, Point, RenderTexture, Sprite } from 'pixi.js';
 import { Stage, Layer } from '@pixi/layers';
 import { GroupMap } from '$/groupMap';
 import { FixedEngine } from '$/engine';
+
+import './src/settings';
+import { WaveFunctionCollapser } from '$/wfc';
 
 async function main() {
 
@@ -100,8 +103,6 @@ async function main() {
     const pathGraphics = new Graphics();
     world.addChild(pathGraphics);
 
-    settings.SCALE_MODE = SCALE_MODES.NEAREST;
-
     Assets.add(
         'pathSheet',
         './lib/assets/world-sheet.json'
@@ -111,7 +112,7 @@ async function main() {
     // console.log(pathSheet);
 
     let level;
-    function generatePath() {
+    async function generatePath() {
 
         const path = world.createPath(-4, 4, new Point(0, -1));
 
@@ -120,7 +121,113 @@ async function main() {
         let maxY = 0;
         let minY = 0;
 
+        const wfc = new WaveFunctionCollapser();
+
+        function forEachAdjacent(x, y, callback) {
+
+            for (let ox = -1; ox <= 1; ox++) {
+
+                for (let oy = -1; oy <= 1; oy++) {
+
+                    if (ox === 0 && ox === oy) {
+
+                        continue;
+
+                    }
+
+                    const nx = x + ox;
+                    const ny = y + oy;
+
+                    callback(nx, ny);
+
+                }
+
+            }
+
+        }
+
+        wfc.createRule('no-adjacent-water', (value, x, y) => {
+
+            let result = true;
+
+            forEachAdjacent(x, y, (nx, ny) => {
+
+                if (wfc.grid.get(nx, ny)?.decision?.value === 'w') {
+
+                    result = false;
+
+                }
+
+            });
+
+            return result;
+
+        });
+
+        wfc.createRule('no-adjacent-land', (value, x, y) => {
+
+            let result = true;
+
+            forEachAdjacent(x, y, (nx, ny) => {
+
+                if (wfc.grid.get(nx, ny)?.decision?.value === 'l') {
+
+                    result = false;
+
+                }
+
+            });
+
+            return result;
+
+        });
+
+        for (let x = -6; x <= 6; x++) {
+
+            for (let y = -6; y <= 6; y++) {
+
+                wfc.setCell(x, y);
+
+                wfc.addOptionToCell(x, y, 'l', ['no-adjacent-water']);
+
+                wfc.addOptionToCell(x, y, 'c', []);
+
+                wfc.addOptionToCell(x, y, 'w', ['no-adjacent-land']);
+
+            }
+
+        }
+
+        const valueColorMap = {
+            l: 0x00FF00,
+            c: 0xFFFF00,
+            w: 0x0000FF
+        };
+
+        wfc.onCellCollapse((decision) => {
+
+            if (!decision) {
+
+                return;
+
+            }
+
+            const { x, y, value } = decision;
+
+            pathGraphics.beginFill(valueColorMap[value] || 0xFFFFFF);
+            pathGraphics.drawRect(
+                (x * world.cellSize.x) - (world.cellSize.x / 2),
+                (y * world.cellSize.y) - (world.cellSize.y / 2),
+                world.cellSize.x, world.cellSize.y
+            );
+            pathGraphics.endFill();
+
+        });
+
         pathGraphics.clear();
+
+        await wfc.collapse();
+
         let i = 0;
         path.forEach((point) => {
 
