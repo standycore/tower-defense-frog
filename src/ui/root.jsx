dcc9420a8627bfc104e2f2368d3b31bcbb0fe010
Matthew <110214849+standycore@users.noreply.github.com>
2022-12-21 05:45:38 -0800

    created root container for reacting ui

---
+++ src/ui/root.jsx
@@ -0,0 +1,12 @@
+import Shop from './Shop/Shop';
+import './root.css';
+
+export default function ReactRoot() {
+
+    return (
+        <div id="root">
+            <Shop/>
+        </div>
+    );
+
+}
