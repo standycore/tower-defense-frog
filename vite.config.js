dce7510942eb1a50c23b02a611d00f54160106b3
Matthew <110214849+standycore@users.noreply.github.com>
2022-12-20 18:28:38 -0800

    modified path resolves

--- vite.config.js
+++ vite.config.js
@@ -6,7 +6,8 @@ export default {
     },
     resolve: {
         alias: {
-            $: path.resolve(__dirname, 'lib')
+            $: path.resolve(__dirname, 'lib'),
+            src: path.resolve(__dirname, './src')
         }
     }
 };
