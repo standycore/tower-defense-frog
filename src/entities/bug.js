05a5adab7ae0d85d045893c216f1e0a85c2657a1
Matthew <110214849+standycore@users.noreply.github.com>
2022-12-20 18:33:16 -0800

    bug entity that will add required components itself

---
+++ src/entities/bug.js
@@ -0,0 +1,57 @@
+import { CustomSprite } from '$/customSprite';
+import { ECS } from '$/ecs';
+import { Assets } from 'pixi.js';
+import { CustomSpriteComponent } from 'src/components/customSprite';
+import { HealthComponent } from 'src/components/health';
+import { PathFollowerComponent } from 'src/components/pathFollower';
+import { WorldComponent } from 'src/components/world';
+
+class Bug extends ECS.Entity {
+
+    constructor(world, type, pathArray) {
+
+        super();
+
+        let sprite;
+        let moveInterval;
+        let health;
+
+        // changes the sprite, the health, and the move speed of the fly based on the type
+        if (type === 'fly') {
+
+            sprite = new CustomSprite(Assets.get('flySheet'));
+            health = 1;
+            moveInterval = 1000;
+
+        } else if (type === 'spider') {
+
+            sprite = new CustomSprite(Assets.get('spiderSheet'));
+            health = 5;
+            moveInterval = 2000;
+
+        } else if (type === 'butterfly') {
+
+            sprite = new CustomSprite(Assets.get('butterflySheet'));
+            health = 3;
+            moveInterval = 500;
+
+        }
+
+        // creates the sprite, and starts the animation
+        sprite.play();
+        sprite.anchor.set(0.5);
+        sprite.width = world.cellSize.x;
+        sprite.height = world.cellSize.y;
+
+        world.addChild(sprite);
+
+        this.addComponent(CustomSpriteComponent, sprite);
+        this.addComponent(WorldComponent, world, sprite);
+        this.addComponent(PathFollowerComponent, pathArray, moveInterval);
+        this.addComponent(HealthComponent, health);
+
+    }
+
+}
+
+export { Bug };
