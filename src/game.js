c0a4b8aac429c970f7348b45beaba148b452c4a4
Matthew <110214849+standycore@users.noreply.github.com>
2022-12-30 07:13:33 -0800

    utilizes Input and world helper methods,  frog line debug,  frog gets good bug target, colored circles

--- src/game.js
+++ src/game.js
@@ -1,14 +1,16 @@
 import { ECS } from '$/ecs';
 import { EventEmitter } from '$/events';
 import { Vector2 } from '$/vector';
-import { Graphics } from 'pixi.js';
+import { Graphics, RenderTexture, Sprite } from 'pixi.js';
 import { loadAssets } from './assets';
 import { FrogComponent } from './components/frog';
 import { HealthComponent } from './components/health';
 import { WorldComponent } from './components/world';
+import { PathFollowerComponent } from './components/pathFollower';
 import { Bug } from './entities/bug';
 import { Frog } from './entities/frog';
 import { Global } from './global';
+import { Input } from './input';
 
 async function load() {
 
@@ -19,10 +21,11 @@ async function load() {
 const entities = [];
 let world;
 let pathArray;
+let level;
 let spawnPoint;
 let currentFrog;
 let graphics;
-const g = new Graphics();
+let liveGraphics;
 
 async function preUpdate() {
 
@@ -32,40 +35,73 @@ async function preUpdate() {
     console.log('setting up world and level');
     world = Global.world;
     pathArray = Global.level.pathArray;
+    level = Global.level;
     spawnPoint = pathArray[0];
     graphics = new Graphics();
+    liveGraphics = new Graphics();
     world.addChild(graphics);
+    world.addChild(liveGraphics);
+
+    // here i am testing using a sprite as a circle
+    // it draws a circle using graphics then renders it onto a texture
+    // the texture is used to create a sprite, which is then added to the world
+    // change the condition below to true to see it
+    // eslint-disable-next-line no-constant-condition
+    if (false) {
+
+        graphics.beginFill(0xFFFFFF, 1);
+        graphics.drawCircle(50, 50, 50);
+
+        // render texture created
+        const renderTexture = RenderTexture.create({ width: 100, height: 100 });
+
+        // render graphics to render texture
+        Global.app.renderer.render(graphics, { renderTexture });
+
+        // create sprite and set anchor/origin to .5
+        const sprite = Sprite.from(renderTexture);
+        sprite.anchor.set(0.5);
+
+        // add to world
+        world.addChild(sprite);
+
+        // wow did you know, sprites also have an alpha property, which lets them be semi transparent
+        sprite.alpha = 0.5;
+
+        graphics.clear();
+
+    }
 
     // Event that controlls the frog eat action
     EventEmitter.events.on('frogEatBug', (frog) => {
 
-        // loops through the entire path array and the entities array, checks to make sure the entity is a bug, then compares the
-        // x and y coordinates. if the coordinates match the furthest tile, the frog will attempt to eat the fly.
-        for (let i = pathArray.length - 1; i >= 0; i--) {
+        // finds the closest bug to the frog and is furthest on the path and is in range of frog
+        let closestBug;
+        let furthestPath = 0;
+        for (let i = 0; i < Bug.array.length; i++) {
 
-            for (let j = 0; j < entities.length; j++) {
+            const bug = Bug.array[i];
 
-                if (entities[j] instanceof Bug) {
+            if (bug.destroyed) {
 
-                    if (pathArray[i].x === Math.round(entities[j].getComponent(WorldComponent).position.x) &&
-                    pathArray[i].y === Math.round(entities[j].getComponent(WorldComponent).position.y)) {
-
-                        if (Vector2.distanceBetweenVectors(entities[j].getComponent(WorldComponent).position,
-                            frog.getComponent(WorldComponent).position) <= frog.getComponent(FrogComponent).range) {
-
-                            entities[j].getComponent(HealthComponent).health -= frog.getComponent(FrogComponent).strength;
-
-                            graphics.lineTo(entities[j].getComponent(HealthComponent).x, entities[j].getComponent(HealthComponent).y);
-                            console.log(entities[j].getComponent(HealthComponent).health);
-                            return;
-
-                        }
-
-                    }
-
-                }
+                continue;
 
             }
+
+            const distance = Vector2.distanceBetweenVectors(bug.getComponent(WorldComponent).position, frog.getComponent(WorldComponent).position);
+            const index = bug.getComponent(PathFollowerComponent).index;
+            if (distance <= frog.getComponent(FrogComponent).range && (!closestBug || (index > furthestPath))) {
+
+                furthestPath = index;
+                closestBug = bug;
+
+            }
+
+        }
+
+        if (closestBug) {
+
+            closestBug.getComponent(HealthComponent).health -= frog.getComponent(FrogComponent).strength;
 
         }
 
@@ -79,38 +115,10 @@ async function preUpdate() {
             id: 'coolfrog',
             name: 'The super cool frog',
             price: 100,
-            callback: async (itemData) => {
+            callback: (itemData) => {
 
                 const frog = new Frog(world, 'frog');
                 currentFrog = frog;
-
-                const handleClick = () => {
-
-                    // console.log(currentFrog);
-                    if (currentFrog) {
-
-                        entities.push(currentFrog);
-                        currentFrog = null;
-                        window.removeEventListener('click', handleClick);
-
-                        g.clear();
-                        graphics.beginFill(0x000000, 0.1);
-                        graphics.drawCircle(frog.getComponent(WorldComponent).position.x * world.cellSize.x,
-                            frog.getComponent(WorldComponent).position.y * world.cellSize.y,
-                            frog.getComponent(FrogComponent).range * world.cellSize.y);
-                        graphics.endFill();
-
-                    }
-
-                };
-
-                await new Promise((resolve) => {
-
-                    setTimeout(resolve, 0);
-
-                });
-
-                window.addEventListener('click', handleClick);
 
             }
         });
@@ -119,39 +127,10 @@ async function preUpdate() {
             id: 'sexyfrog',
             name: 'The super sexy frog',
             price: 120,
-            callback: async (itemData) => {
+            callback: (itemData) => {
 
                 const frog = new Frog(world, 'fast-frog');
                 currentFrog = frog;
-
-                const handleClick = () => {
-
-                    console.log(currentFrog);
-                    if (currentFrog) {
-
-                        entities.push(currentFrog);
-                        currentFrog = null;
-                        window.removeEventListener('click', handleClick);
-
-                        // sets up the range of the frog
-                        g.clear();
-                        graphics.beginFill(0x000000, 0.1);
-                        graphics.drawCircle(frog.getComponent(WorldComponent).position.x * world.cellSize.x,
-                            frog.getComponent(WorldComponent).position.y * world.cellSize.y,
-                            frog.getComponent(FrogComponent).range * world.cellSize.y);
-                        graphics.endFill();
-
-                    }
-
-                };
-
-                await new Promise((resolve) => {
-
-                    setTimeout(resolve, 0);
-
-                });
-
-                window.addEventListener('click', handleClick);
 
             }
 
@@ -167,32 +146,115 @@ let spawnTimer = 5000;
 
 function update(delta, time) {
 
-    world.addChild(g);
+    // clears the live graphics to be updated below
+    // must be cleared every frame so previous frame's drawings dont stick
+    liveGraphics.clear();
 
     if (currentFrog) {
 
-        // sets up the range of the frog
-        g.clear();
-        g.beginFill(0x000000, 0.1);
-        g.drawCircle(currentFrog.getComponent(WorldComponent).position.x * world.cellSize.x,
-            currentFrog.getComponent(WorldComponent).position.y * world.cellSize.y,
-            currentFrog.getComponent(FrogComponent).range * world.cellSize.y);
-        g.endFill();
+        const frogComponent = currentFrog.getComponent(FrogComponent);
+        const frogWorldComponent = currentFrog.getComponent(WorldComponent);
+        const frogCanvasPosition = world.worldToCanvasPosition(frogWorldComponent.position);
 
-        currentFrog.getComponent(WorldComponent).position.set(Math.round((Global.canvasMousePosition.x - Global.app.view.width / 2) / world.cellSize.x), Math.round((Global.canvasMousePosition.y - Global.app.view.height / 2) / world.cellSize.y));
+        // converts canvas position to world position
+        const mouseWorldPosition = world.canvasToWorldPosition(Input.canvasMousePosition);
+
+        // snaps world position by rounding it
+        const snappedWorldPosition = new Vector2(Math.round(mouseWorldPosition.x), Math.round(mouseWorldPosition.y));
+
+        // set the floating frogs position to the snapped position
+        frogWorldComponent.position.set(snappedWorldPosition.x, snappedWorldPosition.y);
+
+        // gets the level's grid's cell from the snapped position
+        const cell = level.grid.get(snappedWorldPosition.x, snappedWorldPosition.y);
+
+        // defines a boolean validCell starting with true, and is invalidated with the following conditions
+        let validCell = true;
+
+        // if there is no cell, then it is not a valid cell
+        if (!cell) {
+
+            validCell = false;
+
+        } else {
+
+            // if cell has a path on it, then it is not a valid cell
+            if (cell.path) {
+
+                validCell = false;
+
+            }
+
+            // if cell has a frog on it, then it is not a valid cell
+            if (cell.frog) {
+
+                validCell = false;
+
+            }
+
+        }
+
+        // sets the circle color of the moving circle based on whether it is a valid cell or not
+        // valid cell? GREEN otherwise: RED
+        const circleColor = validCell ? 0x44FF44 : 0xFF4444;
+
+        liveGraphics.beginFill(circleColor, 0.2);
+        liveGraphics.drawCircle(
+            frogCanvasPosition.x,
+            frogCanvasPosition.y,
+            frogComponent.range * world.cellSize.y);
+        liveGraphics.endFill();
+
+        // when clicked, only if the cell is valid will the frog be placed
+        if (Input.justClicked() && validCell) {
+
+            // pushes the currentFrog to the entities array
+            // this allows it to start updating
+            entities.push(currentFrog);
+
+            // draws a "permanent" light black circle around the frog indicating its bug eating range
+            graphics.beginFill(0x000000, 0.1);
+            graphics.drawCircle(frogCanvasPosition.x,
+                frogCanvasPosition.y,
+                frogComponent.range * world.cellSize.y);
+            graphics.endFill();
+
+            // stores the frog in the cell to be accessed and checked against via positions
+            cell.frog = currentFrog;
+
+            // make currentFrog null to clear the mouse selection
+            currentFrog = null;
+
+        }
 
     }
 
+    // updates all entities in the entities array and removes them from the array if they are destroyed every frame
     for (let i = 0; i < entities.length; i++) {
 
         const entity = entities[i];
         if (entity.destroyed) {
 
             entities.splice(i, 1);
+            i -= 1;
 
         } else {
 
             ECS.updateEntity(entity, delta, time);
+
+        }
+
+    }
+
+    // removes bugs from the bugs array when they are destroyed
+    // this might be changed to a better way in the future
+    for (let i = 0; i < Bug.array.length; i++) {
+
+        const bug = Bug.array[i];
+        if (bug.destroyed) {
+
+            Bug.array.splice(i, 1);
+            i -= 1;
 
         }
 
@@ -203,7 +265,8 @@ function update(delta, time) {
 
         spawnTimer -= 2500;
 
-        const bug = new Bug(world, bugType[Math.floor(Math.random() * bugType.length)], pathArray);
+        const randomIndex = Math.floor(Math.random() * bugType.length);
+        const bug = new Bug(world, bugType[randomIndex], pathArray);
 
         entities.push(bug);
 
@@ -212,6 +275,51 @@ function update(delta, time) {
     }
 
     spawnTimer += delta;
+
+    // frog targeting debug
+
+    // sets line style to draw red lines
+    // this is for the debugging of frogs to see which bug they are targeting (below)
+    liveGraphics.lineStyle(1, 0xFF0000, 1);
+
+    entities.forEach((entity) => {
+
+        if (entity instanceof Frog) {
+
+            const frog = entity;
+
+            let closestBug;
+            let furthestPath = 0;
+            for (let i = 0; i < Bug.array.length; i++) {
+
+                const bug = Bug.array[i];
+
+                const distance = Vector2.distanceBetweenVectors(bug.getComponent(WorldComponent).position, frog.getComponent(WorldComponent).position);
+                const index = bug.getComponent(PathFollowerComponent).index;
+                if (distance <= frog.getComponent(FrogComponent).range && (!closestBug || (index > furthestPath))) {
+
+                    furthestPath = index;
+                    closestBug = bug;
+
+                }
+
+            }
+
+            if (!closestBug) {
+
+                return;
+
+            }
+
+            const a = world.worldToCanvasPosition(frog.getComponent(WorldComponent).position);
+            liveGraphics.moveTo(a.x, a.y);
+
+            const b = world.worldToCanvasPosition(closestBug.getComponent(WorldComponent).position);
+            liveGraphics.lineTo(b.x, b.y);
+
+        }
+
+    });
 
 }
 
