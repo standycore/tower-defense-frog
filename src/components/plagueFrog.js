9dcfce5e5a7f133df069daa73f32b15723dc0728
Matthew <110214849+standycore@users.noreply.github.com>
2023-01-03 21:33:43 -0800

    added plague frog damage

--- src/components/plagueFrog.js
+++ src/components/plagueFrog.js
@@ -1,6 +1,26 @@
+import { Vector2 } from '$/vector';
+import { BugComponent } from './bug';
 import { FrogComponent } from './frog';
 
 class PlagueFrogComponent extends FrogComponent {
+
+    getTargetBugs(bugs) {
+
+        return bugs.filter((bug) => {
+
+            const bugComponent = bug.getComponent(BugComponent);
+            return bugComponent.state === 'alive' && Vector2.distanceBetweenVectors(bugComponent.worldComponent.position, this.worldComponent.position) < this.range;
+
+        });
+
+    }
+
+    onAttack(bug) {
+
+        const bugComponent = bug.getComponent(BugComponent);
+        bugComponent.damage(this.strength);
+
+    }
 
 }
 
