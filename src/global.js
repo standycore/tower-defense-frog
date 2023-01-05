f4ff4cf7843e386f3211d6ab695fc6176ef2379b
Matthew <110214849+standycore@users.noreply.github.com>
2023-01-05 06:01:50 -0800

    added interface in console for global settings, like enabling/disabling debug

--- src/global.js
+++ src/global.js
@@ -1,3 +1,10 @@
-const Global = {};
+/** @type {{public: Object, world: import('$/world').World}} */
+const Global = {
+    public: {
+        debug: false
+    }
+};
+
+window.Global = Global.public;
 
 export { Global };
